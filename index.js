3fd197f84a73664f426aaa0c853ea61b8a6b837a
volha <v.khviadchenia@gmail.com>
2020-04-06 19:47:13 +0200

    feat: generate Title and TextArea through JS

---
+++ index.js
@@ -0,0 +1,14 @@
+const title = document.createElement('h1');
+title.innerHTML = 'virtual keyboard for Windows';
+title.classList.add('title');
+document.body.appendChild(title);
+
+const switchLang = document.createElement('h3');
+switchLang.innerHTML = 'для смены раскладки языка используйте...';
+switchLang.classList.add('switchLang');
+document.body.appendChild(switchLang);
+
+const textArea = document.createElement('textArea');
+textArea.classList.add('use-keyboard-input', 'textArea');
+textArea.style
+document.body.appendChild(textArea);
